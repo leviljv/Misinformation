494db4cdbff61f56d62dd0007e47fb40cdbf24e2
BartVanLierop <bartvanlierop.bvl@gmail.com>
2021-12-01 09:08:37 +0100

    Added scripts for al 6 color buttons
    Still need to assign the functions to the buttons

--- Assets/Scripts/TextButton.cs
+++ Assets/Scripts/TextButton.cs
@@ -31,7 +31,29 @@ public class TextButton : MonoBehaviour
             GetComponent<Image>().color = textColor;
             textColor = Color.green;
         }
-        else if (uiManager.greenPressed == true && textColor == Color.green || uiManager.bluePressed == true && textColor == Color.blue)
+        else if (uiManager.yellowPressed == true && textColor != Color.yellow)
+        {
+            textColor = Color.yellow;
+            textColor.a = 0.36f;
+            GetComponent<Image>().color = textColor;
+            textColor = Color.yellow;
+        }
+        else if (uiManager.redPressed == true && textColor != Color.red)
+        {
+            textColor = Color.red;
+            textColor.a = 0.36f;
+            GetComponent<Image>().color = textColor;
+            textColor = Color.red;
+        }
+        else if (uiManager.magentaPressed == true && textColor != Color.magenta)
+        {
+            textColor = Color.magenta;
+            textColor.a = 0.36f;
+            GetComponent<Image>().color = textColor;
+            textColor = Color.magenta;
+        }
+        else if (uiManager.whitePressed == true || uiManager.greenPressed == true && textColor == Color.green || uiManager.bluePressed == true && textColor == Color.blue
+             || uiManager.yellowPressed == true && textColor == Color.yellow || uiManager.redPressed == true && textColor == Color.red || uiManager.magentaPressed == true && textColor == Color.magenta)
         {
             GetComponent<Image>().color = standardColor;
             textColor = standardColor;
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -12,6 +12,10 @@ public class UIManager : MonoBehaviour
 
     public bool bluePressed = false;
     public bool greenPressed = false;
+    public bool yellowPressed = false;
+    public bool redPressed = false;
+    public bool magentaPressed = false;
+    public bool whitePressed = false;
 
     void Start()
     {
@@ -27,12 +31,59 @@ public class UIManager : MonoBehaviour
     {
         greenPressed = false;
         bluePressed = true;
+        yellowPressed = false;
+        redPressed = false;
+        magentaPressed = false;
+        whitePressed = false;
     }
 
     public void greenPress()
     {
         greenPressed = true;
         bluePressed = false;
+        yellowPressed = false;
+        redPressed = false;
+        magentaPressed = false;
+        whitePressed = false;
     }
 
+    public void yellowPress()
+    {
+        greenPressed = false;
+        bluePressed = false;
+        yellowPressed = true;
+        redPressed = false;
+        magentaPressed = false;
+        whitePressed = false;
+    }
+
+    public void redPress()
+    {
+        greenPressed = false;
+        bluePressed = false;
+        yellowPressed = false;
+        redPressed = true;
+        magentaPressed = false;
+        whitePressed = false;
+    }
+
+    public void magentaPress()
+    {
+        greenPressed = false;
+        bluePressed = false;
+        yellowPressed = false;
+        redPressed = false;
+        magentaPressed = true;
+        whitePressed = false;
+    }
+
+    public void whitePress()
+    {
+        greenPressed = false;
+        bluePressed = false;
+        yellowPressed = false;
+        redPressed = false;
+        magentaPressed = false;
+        whitePressed = true;
+    }
 }
