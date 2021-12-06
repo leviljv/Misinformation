218687842fcbdc370842257429096d091398f22d
BartVanLierop <bartvanlierop.bvl@gmail.com>
2021-12-06 15:37:23 +0100

    Script analysis + color buttons
    Script analysis ready for dialogue + hover pop up color buttons

--- Assets/Scripts/NextPreviousText.cs
+++ Assets/Scripts/NextPreviousText.cs
@@ -6,6 +6,21 @@ public class NextPreviousText : MonoBehaviour
 {
     public int level;
 
+    private void Start()
+    {
+        if (PlayerStats.Node1 == 2 || PlayerStats.Node1 == 3 )
+        {
+            if (PlayerStats.Node2 == 2)
+            {
+                level = 1;
+            }
+        }
+        else
+        {
+            level = 2;
+        }
+    }
+
     void Update()
     {
         
