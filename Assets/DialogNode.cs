2cce9cf8b8510f164530749ddbdcd9549dd8e4f7
Kim <bl4ck.z0ne.149@gmail.com>
2021-12-07 13:16:33 +0100

    KimScene met dialoog af. Compleet werkend (alst goed is.) Wel nog met oude art

--- Assets/DialogNode.cs
+++ Assets/DialogNode.cs
@@ -6,10 +6,10 @@ public class DialogNode : MonoBehaviour
 {
     public DialogManager refToDialogManager;
     public GameObject refToGameObject;
-    public int targetIntWhenOnMin, targetIntWhenOnMax;
+    public int targetIntWhenOnMin, targetIntWhenOnMax, addToMaxInt; //whenonmax =  after what treshold the text is off, addToMaxInt can be filled in to not have to manually fill in each int, but just adds a number assigned in the inspector to the max int (so for a 2opt choice it woulld be +2)
     void Start()
     {
-        
+        targetIntWhenOnMax = targetIntWhenOnMin + addToMaxInt; //leave addtomaxint blanc if you want to fill everything outyoutself
     }
 
     
@@ -32,11 +32,13 @@ public class DialogNode : MonoBehaviour
     void EnableThisDialog()
     {
         refToGameObject.SetActive(true);
+        Debug.Log("on");
     }
 
     void DisableThisDialog()
     {
         refToGameObject.SetActive(false);
+        Debug.Log("off");
 
     }
 }
